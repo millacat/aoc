60e1388c188a79f251bc22da59a5afea4f41a1e2
millacat <camillakd@msn.com>
2021-12-03 19:04:38 +0100

    Day 3, part 1

---
+++ 3.fsx
@@ -0,0 +1,76 @@
+open IOz
+
+(* It's Christmas, you know *)
+
+(* Part 1. Transpose! *)
+let stringToCharList (s : string) : char List = [for c in s do yield c]
+
+let aListToString l =
+    let l' = List.rev l
+    let b = List.fold (fun acc x -> (string x) + acc) "" l'
+    in "0b" + b
+
+let mult x y = (int x) * (int y)
+
+let data : char List List = getData "3.txt" stringToCharList
+let transposed = List.transpose data
+
+let counts' = List.map (fun originalColumn ->
+                 List.fold (fun (z,o) n -> if n = '1'
+                                           then (z,o+1)
+                                           else (z+1,o))
+                            (0,0) originalColumn
+                       ) transposed
+
+let gamma' = List.map (fun (z,o) -> if z > o then 0 else 1) counts'
+                |> aListToString
+
+let epsilon' = List.map (fun (z,o) -> if z > o then 1 else 0) counts'
+                |> aListToString
+
+let res' = mult gamma' epsilon'
+prn <| res'
+
+(* Part 1 again. No transpose {*.*}  *)
+
+let rec initList (n : int) : (int*int) List =
+    if n > 0
+    then (0,0) :: initList (n-1)
+    else []
+
+let traverse (data : char List List) : (int*int) List =
+
+    let rec count (binary : char List) (zeroOne : (int*int) List)
+                 : (int*int) List =
+        match binary, zeroOne with
+        | '0'::rs, (z,o)::zo -> (1+z, o) :: count rs zo
+        | '1'::rs, (z,o)::zo -> (z, 1+o) :: count rs zo
+        | _, _ -> []
+
+    let rec start data zeroOnes : (int*int) List =
+        match data with
+        | [] -> zeroOnes
+        | str::rest -> let newZeroOnes = count str zeroOnes
+                       in start rest newZeroOnes
+
+    let len = data.[0].Length
+    start data (initList len)
+
+let counts : (int*int) List = traverse data
+
+// The most common bits
+let gamma = List.map (fun (z,o) -> if z > o then 0 else 1) counts
+                |> aListToString
+
+// The least common bits
+let epsilon = List.map (fun (z,o) -> if z > o then 1 else 0) counts
+                |> aListToString
+
+let res = mult gamma epsilon
+prn res
+
+(* Part Two *)
+
+
+
+
