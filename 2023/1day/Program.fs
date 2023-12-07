fe0adbec642ec46ba88d572359c225298a9a97b6
millacat <ckdalsgaard@pm.me>
2023-12-07 21:49:39 +0100

    Day 1 - MERRY CHRISTMAS!

--- 2023/1day/Program.fs
+++ 2023/1day/Program.fs
@@ -3,84 +3,50 @@
 let prn n = printfn $"%A{n}"
 let input = IO.File.ReadLines "input"
 
-let findFirst s = Seq.find Char.IsDigit s
-let findLast s = Seq.findBack Char.IsDigit s
-let getCalibrationValue s = 
-    $"{findFirst s}{findLast s}"
-    |> int
+module part1 =
+    let findFirst s = Seq.find Char.IsDigit s |> string
+    let findLast s = Seq.findBack Char.IsDigit s |> string
+    let getCalibrationValue s = findFirst s + findLast s |> int
 
-let day1_1 =
-    input
-    |> Seq.sumBy getCalibrationValue
-    |> prn
+    let execute =
+        input
+        |> Seq.sumBy getCalibrationValue
+        |> prn
 
-let findFirstIndex s = Seq.findIndex Char.IsDigit s
-let findLastIndex s = Seq.findIndexBack Char.IsDigit s
-let getIndex index (s: string) = s[index]
-let strTodigit = function
-    | "one" -> 1
-    | "two" -> 2
-    | "three" -> 3
-    | "four" -> 4
-    | "five" -> 5
-    | "six" -> 6
-    | "seven" -> 7
-    | "eight" -> 8
-    | "nine" -> 9
-    | _ -> failwith "Should not happen. :)"
+module part2 =
+    let strToInt = function
+        | "one"   -> 1
+        | "two"   -> 2
+        | "three" -> 3
+        | "four"  -> 4
+        | "five"  -> 5
+        | "six"   -> 6
+        | "seven" -> 7
+        | "eight" -> 8
+        | "nine"  -> 9
+        | _ -> failwith "Should not happen. :)"
+    let digits = [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
+    let findFirstNumberIndex s = Seq.findIndex Char.IsDigit s
+    let findLastNumberIndex s = Seq.findIndexBack Char.IsDigit s
 
-let digits = [
-    "one"
-    "two"
-    "three"
-    "four"
-    "five"
-    "six"
-    "seven"
-    "eight"
-    "nine"
-]
+    let getCalibrationValue (s : string) =
+        let containedDigits = digits |> List.filter s.Contains
+        if containedDigits.IsEmpty
+        then part1.getCalibrationValue s
+        else
+            let first,firstsIndex = containedDigits |> List.map (fun digit -> digit |> strToInt, s.IndexOf digit ) |> List.minBy snd
+            let last,lastsIndex   = containedDigits |> List.map (fun digit -> digit |> strToInt, s.LastIndexOf digit) |> List.maxBy snd
+            let tens =
+                let index = findFirstNumberIndex s in
+                if index < firstsIndex then part1.findFirst s |> int else first
+            let ones =
+                let index = findLastNumberIndex s in
+                if index > lastsIndex then part1.findLast s |> int else last
+            10*tens+ones
+    let execute =
+        input
+        |> Seq.sumBy getCalibrationValue
+        |> prn
 
-let getCalibrationValue2 (s : string) =
-    //let indexed = List.indexed >> Seq.toList
-    digits
-    |> List.map (fun digit -> digit, s.IndexOf digit, s.LastIndexOf digit)
-    //|> List.indexed
-    //|> List.unzip
-    //|> (fun (firsts, lasts) -> ())
-
-let mutable count = 0
-let getCalibrationValue3 (s : string) =
-        let digitsInString = digits |> List.filter (fun digit -> s.IndexOf digit > 0 )
-        prn digitsInString
-        count <- count+1
-        prn count
-        let firsts = digitsInString |> List.map (fun digit -> digit, s.IndexOf digit )
-        let lasts = digitsInString |> List.map (fun digit -> digit, s.LastIndexOf digit)
-        let first,_ = firsts |> List.minBy snd
-        let last,_ = lasts |> List.maxBy snd
-        $"{ first |> strTodigit }{ last |> strTodigit }"
-        |> int
-
-let head = input |> Seq.head
-
-let day1_2 =
-    head |> prn
-
-    head
-    |> getCalibrationValue3
-    |> prn
-
-    "248onefouronexcjbbqpcfb"
-    |> getCalibrationValue3
-    |> prn
-
-    // head
-    // |> prn
-
-    IO.File.ReadLines "input"
-    |> Seq.sumBy getCalibrationValue3
-    |> prn
-
-day1_1
-day1_2
+part1.execute
+part2.execute
