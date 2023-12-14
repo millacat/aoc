105ae0def938d9235a70f330fd27a955fb028a53
millacat <ckdalsgaard@pm.me>
2023-12-14 22:18:33 +0100

    Day 3 start - get symbol positions

---
+++ 2023/3day/Program.fs
@@ -0,0 +1,34 @@
+﻿
+open System
+
+let prn n = printfn $"%A{n}"
+let input = IO.File.ReadLines "input" |> Array.ofSeq
+let lineLength = input[0].Length
+type Point = { x : int; y : int }
+
+type Symbols = Point list
+
+let SYMBOLS = "@*-_+$/&%=#!^"
+let isSymbol (c : char) = SYMBOLS.Contains c && c <> '.'
+
+let getSymbolPositions input =
+    input
+    |> Array.indexed
+    |> Array.fold (fun positions (xIndex, row) -> 
+                        let yIndices = 
+                            row 
+                            |> Seq.indexed
+                            |> Seq.filter (snd >> isSymbol)
+                            |> Seq.map fst
+                            |> List.ofSeq
+                        let newPositions = 
+                            (yIndices, List.init yIndices.Length (fun _ -> xIndex)) 
+                            ||> List.zip                        // |> Array.zip (Array.map snd newSymbols)
+                        positions @ newPositions) []
+
+
+let syms = 
+    input
+    |> getSymbolPositions
+
+syms |> prn
