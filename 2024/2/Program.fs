54d8e96dfae07fd9f87b36dea634064a06bd1932
millacat <ckdalsgaard@pm.me>
2024-12-02 21:37:46 +0100

    Day 2 - Part 2

--- 2024/2/Program.fs
+++ 2024/2/Program.fs
@@ -24,12 +24,11 @@ let rec safe f comp = function
     | []
     | [_] -> true
     | l :: l' :: ls -> 
-        let diff = l - l' |> f 
-        comp l l' && 1 <= diff && diff <= 3 && safe f comp (l'::ls)
+        let d = l - l' |> f
+        comp l l' && 1 <= d && d <= 3 && safe f comp (l' :: ls)
 
 let safeDecrease = safe id (>)
 let safeIncrease = safe Math.Abs (<)
-
 let isSafe report = safeDecrease report || safeIncrease report
 
 let result1 = 
@@ -39,6 +38,16 @@ let result1 =
 
 printfn $"Day 2, first result: {result1}"
 
+(* Day 2 - Part 2 - Very Merry Christmassis! *)
+let flip f x y = f y x
+let result2 = 
+    reports
+    |> List.map (fun report -> report :: ([0..report.Length - 1] |> List.map (flip List.removeAt report)))
+    |> List.map (List.exists isSafe)
+    |> List.filter id
+    |> List.length
+
+
 // Test Part 1
 let inc = [1;2;3;5;8]
 let incSame = [1;3;4;4;6]
@@ -46,6 +55,7 @@ let incDec = [1; 3; 2; 5]
 let dec = [8; 5; 2; 1]
 let decSame = [8; 5; 2; 2]
 let decInc = [9; 6; 6; 3]
+
 let testReports = [inc; incSame; incDec; dec; decSame; decInc]
 let expected = [true; false; false; true; false; false]
 
@@ -53,6 +63,3 @@ let r =
     testReports
     |> List.map isSafe
     |> List.iter2 (fun r exp -> printfn $"result: {r}, expected: {exp}") expected
-
-(* Day 2 - Part 2 - Very Merry Christmassis! *)
-
